97bad972c6651ffc48b564cc45d098f397571ad4
Derek Prince <ace.prince08@gmail.com>
2016-04-09 16:29:31 -0600

    Just going through the motions

---
+++ Playthings/primeexamples.py
@@ -0,0 +1,16 @@
+# allright, allright, allright. Let's do some of this random shit.
+# I wonder if that introductory programming class has their assignments posted
+# fo free on their course website
+# it's much harder to come up with ideas than it is to actually flesh them out
+# ain't that fucked up.
+# it's usually completely different in everything else, ideas are worthless.
+for i in range(2,10):
+    for x in range(2,i):
+        if i % x == 0:
+            print(i, "equals ", x, "*", i//x)
+            break
+    else:
+        # the loop-de-doo fails.
+        # even if Atom take 9 years to start up, this is much faster than the terminal. Tabs fer days.
+        print(i, "is a prime number")
+# sweet baby crust I miss shell. HOW DOES DOS NOT HAVE ls. What am I supposed to spam as I idly think of what to do next?
