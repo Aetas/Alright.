f5539bc9fcfcdbbdf4f72d5aedae312b5007d023
Derek Prince <ace.prince08@gmail.com>
2016-04-09 16:38:05 -0600

    You send me right round...

--- Playthings/continuensuch.py
+++ Playthings/continuensuch.py
@@ -1,5 +1,10 @@
 # this proves that 'continue' is indeed a thing languages should have
-for num in range(2,10):
+end = 10
+for num in range(2,end):
+    if num == (end - 1):
+        print("End of the line bub. Take your ", num, " and go.")
+        # continue # would break behave any differently?
+        break # nope
     if num % 2 == 0: # white space, slayer of men.
         print("This numbah is divisible by 2!", num)
         continue # pass go, do not collect allowance
