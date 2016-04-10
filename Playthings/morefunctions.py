df407ca31b029d51ed35dbd1eab453f7eb26f2aa
Derek Prince <ace.prince08@gmail.com>
2016-04-10 16:57:00 -0600

    dictionaries are a bit odd.

--- Playthings/morefunctions.py
+++ Playthings/morefunctions.py
@@ -13,3 +13,20 @@ parrot('a thousand', state='pushing up the daisies')
 
 # interesting, so this is how one gets around the order of arguments and specifying initials at opposite ends of the call.
 # what's better is that the order does not matter.
+
+
+def cheeseshop(kind, *arguments, **keywords):
+    print("-- Do you have any", kind, "?")
+    print("-- I'm sorry, we're all out of", kind)
+    for arg in arguments:
+        print(arg)
+    print("-" * 40)
+    keys = sorted(keywords.keys())
+    for kw in keys:
+        print(kw, ":", keywords[kw])
+
+cheeseshop("Limburger", "It's very runny, sir.",
+           "It's really very, VERY runny, sir.",
+           shopkeeper="Michael Palin",
+           client="John Cleese",
+           sketch="Cheese Shop Sketch")
