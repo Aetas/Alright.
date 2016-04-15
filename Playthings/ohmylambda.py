ba28c96d1943efe9c0d51fb87343f17444e715cb
Derek Prince <ace.prince08@gmail.com>
2016-04-14 20:46:51 -0600

    Just playing around with snytax and frames

---
+++ Playthings/ohmylambda.py
@@ -0,0 +1,15 @@
+# time to do some of them lambda/closure functions n such.
+# Also, VisualStudio Code?
+# On a side note, it seems like people go "You know what sounds like fun?!? Making a language.
+# Mine will be more perfect than the rest because there are no drawbacks to any descision ever. Yeah."
+# And then they go "That's too hard. Let's made an IDE. If that's too much then I'll strip out almost everything and market it like the new Vim."
+# And so here we are.
+
+# Back to lambdas. Which I feel like I finally understand.
+# It's remarkable how complicated people can make things sound when the idea is unremarkable simple.
+
+def overused(n):
+    return lambda: x: x-n
+
+you_must_go_back = overused(100)
+overused(0)
